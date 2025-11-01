db15817c0739efaacbce44c4f3a2a2f50f1388e9
Arthur Arend <arthur.nox@gmail.com>
2025-11-01 17:41:36 -0300

    chamada de busca de filme e inputo de pesquisa

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { Card } from "./components/Card/Card.component";
-import { getTrendingMovies } from "./services/movies.service";
+import { getTrendingMovies, searchMovies } from "./services/movies.service";
 
 function App() {
-  const [count, setCount] = useState(0);
   const [movies, setMovies] = useState<any[]>([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     getTrendingMovies().then((movies) => {
       setMovies(movies);
     });
-  }, [count]);
+  }, []);
+
+  const handleSearchMovies = () => {
+    searchMovies(search).then((movies) => {
+      setMovies(movies);
+    });
+  };
 
   return (
     <>
+      <input
+        type="text"
+        placeholder="Search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button onClick={() => handleSearchMovies()}>Search</button>
       {movies.map((movie) => (
         <Card key={movie.id} movie={movie} />
       ))}
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
--- src/services/movies.service.ts
+++ src/services/movies.service.ts
@@ -12,3 +12,18 @@ export const getTrendingMovies = async () => {
   );
   return response.data.results;
 };
+
+export const searchMovies = async (query: string) => {
+  const response = await axios.get(
+    `${
+      import.meta.env.VITE_BASE_URL
+    }/search/movie?query=${query}&language=en-US`,
+    {
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+      },
+    }
+  );
+  return response.data.results;
+};
